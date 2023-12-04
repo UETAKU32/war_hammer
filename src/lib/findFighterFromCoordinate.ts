37cec7a9a2ddea836045ccf9ef79c8f05b47afa3
上原拓也 <ueharatakuya@ueharatakuyanoMacBook-Air.local>
2023-12-04 18:53:16 +0900

    座標からファイターを見つける関数を作成

---
+++ src/lib/findFighterFromCoordinate.ts
@@ -0,0 +1,12 @@
+import { useGameInfo } from '../hooks/useGameInfo';
+import { Coordinate, } from "../types/Coordinate";
+import { Fighter } from "../types/fighter";
+
+export function findFighterFromCoordinate ({row,col}:Coordinate):Fighter|undefined {
+    const { gameInfo } = useGameInfo();
+    const foundFighter:Fighter|undefined = gameInfo.teams
+    .flatMap(team => team.fighters.filter(fighter => fighter.currentHp > 0))
+    .find(fighter => row === fighter.coordinate.row && col === fighter.coordinate.col);
+
+return foundFighter;
+}
