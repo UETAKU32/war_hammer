0bb00f66405684a9483cd24b2d625830a44ab3d7
上原拓也 <ueharatakuya@ueharatakuyanoMacBook-Air.local>
2023-12-28 13:24:41 +0900

    アクションメニューのUIを選択をわかりやすいように変更

--- src/components/ActionIcon.tsx
+++ src/components/ActionIcon.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { CenterPoint } from '../types/CenterPoint';
 import { hexWidth } from '../lib/hexSize';
+import { useGameInfo } from '../hooks/useGameInfo';
 
 interface ActionIconProps {
     action: Action
@@ -14,6 +15,14 @@ const iconSize: number = hexWidth / 3
 
 const ActionIcon: FC<ActionIconProps> = ({ action, point, onClick }) => {
 
+    const { phase } = useGameInfo();
+
+    let opancy: number = 0.6;
+
+    if (action === "Attack" && phase === "SELECT_ATTACK" || action === "Move" && phase === "SELECT_MOVE") {
+        opancy = 1;
+    }
+
     return (
         <image
             x={point.x - iconSize / 2}
@@ -23,6 +32,7 @@ const ActionIcon: FC<ActionIconProps> = ({ action, point, onClick }) => {
             xlinkHref={`${process.env.PUBLIC_URL}/icons/${action}Action.png`}
             style={{ cursor: "pointer" }}
             onClick={onClick}
+            opacity={opancy}
         />
     )
 }
